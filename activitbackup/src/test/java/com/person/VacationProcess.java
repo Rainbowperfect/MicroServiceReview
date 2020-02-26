b22fcaed29403290da8c62d1c204aa284f851812
RainbowPerfect <903933616@qq.com>
2020-02-27 01:46:09 +0800

    创建并且启动流程实例

--- activitbackup/src/test/java/com/person/VacationProcess.java
+++ activitbackup/src/test/java/com/person/VacationProcess.java
@@ -7,10 +7,13 @@ import org.activiti.engine.RuntimeService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.task.TaskQuery;
 import org.junit.Test;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.util.List;
 
 /**
  * 简单的流程图
@@ -21,30 +24,31 @@ import java.net.URL;
  */
 
 public class VacationProcess {
+    // 获取流程引擎
     ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
 
+    /**
+     * 定义流程实例
+     */
     @Test
-    public void deployTest () {
-        RepositoryService service = processEngine.getRepositoryService();
-        DeploymentBuilder builder = service.createDeployment();
-        System.out.println("builder:===="+builder);
-        InputStream in = this.getClass().getClassLoader().getResourceAsStream("diagrams/helloworld.bpmn ");
-        // builder.addClasspathResource("resources/diagrams/HelloWord.xml");
-        DeploymentBuilder deployment = builder.addInputStream("diagrams/helloworld", in);
-        // URL resource = this.getClass().getClassLoader().getResource("HelloWord.bpmn");
-        deployment.deploy();
+    public void deployTest () throws Exception{
+
+        String filePath = "/diagrams/HelloWord.bpmn";
+        InputStream is = InitTable.class.getClass().getResource(filePath).openStream();
+        Deployment deploy = processEngine.getRepositoryService().createDeployment().name("请假流程开始").addInputStream(
+                "/diagrams/QingJProcess.bpmn",is).deploy();
+        System.out.println("名字" + deploy.getName() +"===" +"流程实例ID"+deploy.getId());
+
     }
 
-    /**部署流程定义*/
+    /**
+     * 启动流程实例
+     */
     @Test
-    public void deploymentProcessDefinition(){
-        Deployment deployment = processEngine.getRepositoryService()//与流程定义和部署对象相关的Service
-                .createDeployment()//创建一个部署对象
-                .name("helloworld入门程序")//添加部署的名称
-                .addClasspathResource("diagrams/helloworld.bpmn")//从classpath的资源中加载，一次只能加载一个文件
-                .addClasspathResource("diagrams/helloworld.png")//从classpath的资源中加载，一次只能加载一个文件
-                .deploy();//完成部署
-        System.out.println("部署ID："+deployment.getId());//1
-        System.out.println("部署名称："+deployment.getName());//helloworld入门程序
+    public void startProcess () {
+        // 对应的act_re_procdef
+        ProcessInstance word = processEngine.getRuntimeService().startProcessInstanceByKey("HelloWord");
+        System.out.println("启动流程实列:" + word.getId());
+
     }
 }
